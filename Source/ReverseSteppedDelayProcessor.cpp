9c50fbbaf6d87b49f3fe6a54ef42b02b160d253a
Radiofloyd <alonso.v.herrera@gmail.com>
2025-08-29 09:13:49 -0400

    primitive reverse ping-pong delay functional

--- Source/ReverseSteppedDelayProcessor.cpp
+++ Source/ReverseSteppedDelayProcessor.cpp
@@ -11,26 +11,21 @@ void ReverseSteppedDelayProcessor::init(int channels, int sampleRate, int delayS
     accumulatedDelay = 0;
 }
 
+
 juce::AudioBuffer<float> ReverseSteppedDelayProcessor::writeMainBuffer(int channel, juce::AudioBuffer<float>& buffer)
 {
     _tempBuffer.makeCopyOf(buffer);
     int bufferSpl = _tempBuffer.getNumSamples();
     int proposedReadPtr = _dWritePtr - (dTime * _internalSampleRate);
     _dReadPtr = (proposedReadPtr >= 0) ? proposedReadPtr : proposedReadPtr + _dSpl;
-    if(_dWritePtr > delayPtr) delayPtr += dTime * _internalSampleRate;
-    DBG(delayPtr);
-    if (delayPtr >= _dSpl) delayPtr = dTime * _internalSampleRate;
-    for (int i = 0; i < bufferSpl; i++)
-    {
-        int currentRead = delayPtr - accumulatedDelay - i;
-        if (currentRead < 0) 
-        {
-            currentRead = _dSpl + accumulatedDelay - i;
-            accumulatedDelay = 0; 
-        }
-        float sample = dBuffer.getSample(channel, currentRead);
-        buffer.setSample(channel, i, sample);
+    if (accumulatedDelay >= dTime * _internalSampleRate) {
+        reverseBuffer(channel, dTime * _internalSampleRate);
+        accumulatedDelay = 0;
     }
+    int loopCopyNum = (_dReadPtr + bufferSpl) > _dSpl ? (_dReadPtr + bufferSpl) % _dSpl : 0;
+    int forwardCopyNum = bufferSpl - loopCopyNum;
+    _tempBuffer.addFrom(channel, 0, dBuffer.getReadPointer(channel, _dReadPtr), forwardCopyNum);
+    _tempBuffer.addFrom(channel, forwardCopyNum, dBuffer.getReadPointer(channel, 0), loopCopyNum);
     return _tempBuffer;
 }
 
@@ -41,6 +36,25 @@ void ReverseSteppedDelayProcessor::performTimeChange(int channel, juce::AudioBuf
         buffer.applyGainRamp(channel, 0, buffer.getNumSamples(), 0, 1);
     }
     buffer.applyGainRamp(channel, 0, buffer.getNumSamples(), 1, 0);
+}
+
+void ReverseSteppedDelayProcessor::incrementWritePointer(int samples)
+{
+    _dWritePtr = (_dWritePtr + samples) % _dSpl;
+    accumulatedDelay += samples;
+}
+
+void ReverseSteppedDelayProcessor::reverseBuffer(int channel, int delaySamples)
+{
+    if (_dReadPtr + delaySamples <= _dSpl) {
+        dBuffer.reverse(channel, _dReadPtr, delaySamples);
+    }
+    else {
+        int forwardCopyNum = _dSpl - _dReadPtr;
+        int loopCopyNum = delaySamples - forwardCopyNum;
+        dBuffer.reverse(channel, _dReadPtr, forwardCopyNum);
+        dBuffer.reverse(channel, 0, loopCopyNum);
+    }
 }
 
 
--- Source/ReverseSteppedDelayProcessor.h
+++ Source/ReverseSteppedDelayProcessor.h
@@ -4,10 +4,14 @@ class ReverseSteppedDelayProcessor :
     public AbsDelayProcessor
 {
 public:
+    bool playingReverse = false;
+    int blockSamplesPlayed = 0;
     int delayPtr = 0;
     int accumulatedDelay = 0;
     void init(int channels, int sampleRate, int delaySamples, int delayTimeSamples) override;
     juce::AudioBuffer<float> writeMainBuffer(int channel, juce::AudioBuffer<float>& buffer) override;
     void performTimeChange(int channel, juce::AudioBuffer<float>& buffer, int time) override;
+    void incrementWritePointer(int samples) override;
+    void reverseBuffer(int channel, int delaySamples);
 };
 
